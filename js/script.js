00d5971a0a7fc9b600b1b1a672f1f92c61739e48
Risolat <utkurova.risolat@mail.ru>
2019-12-02 13:59:51 +0500

    Modify the script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-// var password = prompt('Paswword ');
-// password = parseInt(password, 10);
-
-// if (password % 3 === 0 && password % 5 === 0) {
-// 	console.log('FizzBuzz');
-// } else if(password % 5 === 0){
-// 	console.log('Buzz');
-// } else if(password % 3 === 0){
-// 	console.log('Fizz');
-// } else{
-// 	console.log(password);
-// }
-
 var shoppingList = [];
 var addProBtn = document.querySelector('.add-product');
 var newProductInput = document.querySelector('.product-name');
